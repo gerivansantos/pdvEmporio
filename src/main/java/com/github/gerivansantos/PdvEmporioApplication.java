e00e817d1fa5644d153469eef43bfeac0540304c
gerivansantos <gerivantpc3@gmail.com>
2018-12-24 08:39:13 -0200

    Projeto Criado

---
+++ src/main/java/com/github/gerivansantos/PdvEmporioApplication.java
@@ -0,0 +1,14 @@
+package com.github.gerivansantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvEmporioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvEmporioApplication.class, args);
+	}
+
+}
+
